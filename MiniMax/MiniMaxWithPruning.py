32768ded6288f170297382e8facb72944fa6e6ed
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2024-12-03 01:17:52 +0200

    Increase nodes expanded in alphabeta

--- MiniMax/MiniMaxWithPruning.py
+++ MiniMax/MiniMaxWithPruning.py
@@ -45,6 +45,7 @@ class MiniMaxWithPruning(MiniMax):
             new_board = self.make_move(board, move, self.opponent(self.player))
             child = Node(float('-inf'), "MAX", move)
             root.add_successor(child)
+            self.nodes_expanded += 1
             score, _ = self.maximize(new_board, depth - 1, child, alpha, beta, extra)
             beta = min(beta, score)
             if score < min_score:
